e37ea36f5cd127c84e5338f92699f2506cb6f382
GaoWeigong <gaoweigong@3ttech.cn>
2019-09-19 18:55:44 +0800

    table api

---
+++ flink-learning-sql-examples/src/main/scala/xyz/anydata/bigdata/flink/sql/examples/model/User.scala
@@ -0,0 +1,10 @@
+package xyz.anydata.bigdata.flink.sql.examples.model
+
+/**
+  * 功能描述 TODO 
+  *
+  * @author CainGao
+  * @version V_1.0
+  * @date 2019/9/19 17:31
+  */
+case class User (name:String,age:Int)
---
+++ flink-learning-sql-examples/src/main/scala/xyz/anydata/bigdata/flink/sql/examples/table/TableApiWordCount.scala
@@ -0,0 +1,39 @@
+package xyz.anydata.bigdata.flink.sql.examples.table
+
+import org.apache.flink.api.scala._
+import org.apache.flink.table.api.scala._
+import org.apache.flink.table.api.Table
+import xyz.anydata.bigdata.flink.sql.examples.model.User
+
+/**
+  * 功能描述 构建Table API 与 执行查询
+  *
+  * @author CainGao
+  * @version V_1.0
+  * @date 2019/9/19 16:35
+  */
+object TableApiWordCount {
+
+  def main(args: Array[String]): Unit = {
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    val tableEnv = BatchTableEnvironment.create(env)
+
+    val dataset:DataSet[User] = env.fromElements(
+      User("CainGao",10),
+      User("CainGao",1),
+      User("CainGao",65)
+    )
+
+    val table:Table = tableEnv.fromDataSet(dataset)
+
+    table.printSchema()
+
+    table
+      .groupBy("name")
+      .select("name,age.sum as ages")
+      .toDataSet[User]
+      .print()
+
+  }
+
+}
